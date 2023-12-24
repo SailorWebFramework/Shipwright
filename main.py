5fcb1c8d708be7228afeae8d5d3b3f823cee9107
Joshua Davis <chess.ofx@gmail.com>
2023-12-24 00:33:18 -0500

    new stuff genrated

--- main.py
+++ main.py
@@ -9,12 +9,23 @@ data_path = os.path.join('.', 'Data')
 
 tags_url = os.path.join(data_path, 'tags.csv')
 attributes_url =  os.path.join(data_path, 'attributes.csv')
+events_url =  os.path.join(data_path, 'events.csv')
+css_props_url = os.path.join(data_path, 'css_properties.csv')
+
 templates_url = os.path.join('.', 'Templates')
+page_template_url = os.path.join(templates_url, 'Page.mustache')
+page_events_template_url = os.path.join(templates_url, 'Page+Events.mustache')
 tag_template_url = os.path.join(templates_url, 'HTMLTag.mustache')
+events_mustache_url =  os.path.join(templates_url, 'Events.mustache')
+css_props_template_url = os.path.join(templates_url, 'Properties.mustache')
 
 out_dir = os.path.join('..', 'Sailor', 'Sources', 'Generated')
 tag_out_dir = os.path.join('..', 'Sailor', 'Sources', 'Generated', 'Tags')
+page_out_dir = os.path.join('..', 'Sailor', 'Sources', 'Generated', 'Page')
+events_out_dir = os.path.join('..', 'Sailor', 'Sources', 'Generated', 'Events')
+css_props_out_dir = os.path.join('..', 'Sailor', 'Sources', 'Generated', 'Properties')
 
+# https://www.w3schools.com/tags/default.asp
 # Generate Tags
 with open(tags_url, newline='') as csvfile:
     reader = csv.reader(csvfile)
@@ -45,6 +56,145 @@ with open(tags_url, newline='') as csvfile:
         of = open(genfile_out,"w")
         of.write(output)
         of.close()
+
+
+
+# https://www.w3schools.com/jsref/dom_obj_event.asp
+# Generate Events
+with open(events_url, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+
+    # skip titles in csv
+    next(reader)
+
+    template_fd = open(events_mustache_url, "r")
+    template = template_fd.read()
+
+    events = []
+
+    for row in reader:
+        name = row[0].strip()
+        typedreturn = row[1].strip()
+        returnvalue = row[2].strip()
+
+        ctype = typedreturn
+
+        if ctype == 'object':
+            ctype = "JSObject"
+
+        events.append({
+            'name': name, 
+            'lname': name.lower(),
+            'ctype': ctype,
+            'ltype': typedreturn.lower(),
+            'evalue': returnvalue,
+            'istyped': returnvalue != 'none' and typedreturn != 'none'
+        })
+
+        args = {
+            'template': template,
+            'data': events[-1]
+        }
+
+        output = chevron.render(**args)
+
+        genfile_out = os.path.join(events_out_dir, "Page+" + "on"+ name + ".swift")
+        of = open(genfile_out,"w")
+        of.write(output)
+        of.close()
+    
+    #TODO: add? write to Page.swift
+    # template_fd = open(page_template_url, "r")
+    # template = template_fd.read()
+
+    # args = {
+    #     'template': template,
+    #     'data': {
+    #         "events": events
+    #     }
+    # }
+
+    # output = chevron.render(**args)
+    
+    # genfile_out = os.path.join(page_out_dir, "InternalGeneratedPage.swift")
+
+    # of = open(genfile_out,"w")
+    # of.write(output)
+    # of.close()
+
+    # template_fd = open(page_events_template_url, "r")
+    # template = template_fd.read()
+
+    # args = {
+    #     'template': template,
+    #     'data': {
+    #         "events": events
+    #     }
+    # }
+
+    # output = chevron.render(**args)
+    # genfile_out = os.path.join(page_out_dir, "Page+Events.swift")
+
+    # of = open(genfile_out,"w")
+    # of.write(output)
+    # of.close()
+
+
+# https://www.w3schools.com/jsref/dom_obj_event.asp
+# Generate Properties
+with open(css_props_url, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+
+    # skip titles in csv
+    next(reader)
+
+    template_fd = open(css_props_template_url, "r")
+    template = template_fd.read()
+
+    events = []
+
+    for row in reader:
+        fname = row[0].strip()
+        name = row[1].strip()
+        unit = row[2].strip()
+
+        events.append({
+            'fname': fname, 
+            'cfname': fname.capitalize(),
+            'name': name,
+            'unit': unit
+        })
+        
+        args = {
+            'template': template,
+            'data': events[-1]
+        }
+
+        output = chevron.render(**args)
+
+        genfile_out = os.path.join(css_props_out_dir, "Style.Property+" + fname + ".swift")
+        of = open(genfile_out,"w")
+        of.write(output)
+        of.close()
+    
+    # #write to Page.swift
+    # template_fd = open(page_template_url, "r")
+    # template = template_fd.read()
+
+    # args = {
+    #     'template': template,
+    #     'data': {
+    #         "events": events
+    #     }
+    # }
+
+    # output = chevron.render(**args)
+    
+    # genfile_out = os.path.join(page_out_dir, "Page.swift")
+
+    # of = open(genfile_out,"w")
+    # of.write(output)
+    # of.close()
 
 # TODO: make Lang attribute integrated not string
 # Generate Attributes
